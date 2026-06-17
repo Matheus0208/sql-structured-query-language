d04110c3f127e84b70e0b0155a650016684baec7
Madoh <matheusaugustohorta@hotmail.com>
2026-06-17 13:59:06 -0300

    Adicionando a questão 1

---
+++ query_questao1.sql
@@ -0,0 +1,5 @@
+-- Selecione, da tabela co2_emissions_pc, todos os países e anos cuja emissão seja igual a 0,2. Observe que os dados estão no formato americano, no qual o . (ponto) é o separador de decimais. Indique se os países Guatemala e Lituânia estão na lista.
+
+SELECT country, ref_year, co2_pc
+FROM co2_emissions_pc
+WHERE co2_pc = '0.2'
