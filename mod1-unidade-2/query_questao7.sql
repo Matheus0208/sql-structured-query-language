ae4cfc24f3e0ff2f3610bc406f428ef8581880d7
Madoh <matheusaugustohorta@hotmail.com>
2026-07-02 13:56:23 -0300

    Adicionando resposta das perguntas 7, 8, 9 da atividade

--- mod1-unidade-2/query_questao7.sql
+++ mod1-unidade-2/query_questao7.sql
@@ -1,7 +1,8 @@
 -- Utilizando a tabela country, selecione os países europeus de renda média baixa. Use a classificação Gapminder de oito regiões geográficas (campo eight_regions com valor europe_east e europe_west), para verificar os países, e a classificação de quatro faixas de renda do Banco Mundial (campo wb4income com o valor Lower middle income), para verificar a renda. Indique se todos os países são do leste europeu.
 
-SELECT *
-FROM co2_emissions_pc
-WHERE ref_year BETWEEN 2019 AND 2021
-AND country IN ('Brazil', 'China', 'EUA')
+SELECT country, eight_regions
+FROM country
+WHERE (eight_regions = 'europe_east')
+OR (eight_regions = 'europe_west')
+AND wb4income = 'Lower middle income'
 
